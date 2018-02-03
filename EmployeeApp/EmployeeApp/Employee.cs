6cf9dda1e798da16d91d97f4e4a1078134559edd
Олесуа и Юля <astahova.98@gmail.com>
2018-02-03 10:56:06 +0700

    Инкапсуляция Employee

---
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        //Поля данных
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        //Конструкторы
+        public Employee() { }
+        public Employee (string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        //Методы
+        public string GetName()
+        {
+            return empName;
+        }
+        public void SetName(string name)
+        {
+            //Перед присваиванием проверить входное значение
+            if (name.Length > 15)
+            {
+                //Ошибка!
+                Console.WriteLine("Error! Name must be less than 16 characters!");
+            }
+            else
+            {
+                empName = name;
+            }
+        }
+        public void GiveBonus (float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplayStats ()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}
